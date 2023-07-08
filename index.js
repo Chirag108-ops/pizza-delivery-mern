59ddbf91fb4db4b156c47eee646d33cc48cad139
Chirag Goyal <chiraggoyal@Chirags-MacBook-Air.local>
2023-07-09 00:06:07 +0530

    cors policy updation

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(session({
 }))
 app.use(
     cors({
-        origin : 'http://localhost:3000',
+        origin : 'https://pizza-delivery-mern-client.vercel.app/',
         credentials : true
     })
 )
